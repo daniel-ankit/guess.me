60e516743222b14cb95e5f616ee8beec88080c47
Ankit Kumar <ankit.kumar.061000@gmail.com>
2023-07-17 00:36:39 +0530

    First commit

---
+++ public/words.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "Programming Language"
+    },
+    {
+        word: "guitar",
+        hint: "A Musical Instrument"
+    },
+    {
+        word: "aim",
+        hint: "A Purpose or Intention"
+    },
+    {
+        word: "venus",
+        hint: "Planet of our Solar System"
+    },
+    {
+        word: "gold",
+        hint: "A Yellow Precious Metal"
+    },
+    {
+        word: "ebay",
+        hint: "Online Shopping Site"
+    },
+    {
+        word: "golang",
+        hint: "Programming Language"
+    },
+    {
+        word: "coding",
+        hint: "Related to Programming"
+    },
+    {
+        word: "matrix",
+        hint: "Science Fiction Movie"
+    },
+    {
+        word: "bugs",
+        hint: "Related to Programming"
+    },
+    {
+        word: "avatar",
+        hint: "Epic Science Fiction Film"
+    },
+    {
+        word: "gif",
+        hint: "A file format for Image"
+    },
+    {
+        word: "mental",
+        hint: "Related to the Mind"
+    },
+    {
+        word: "map",
+        hint: "Diagram Represent of an Area"
+    },
+    {
+        word: "island",
+        hint: "Land surrounded by Water"
+    },
+    {
+        word: "hockey",
+        hint: "A famous outdoor Game"
+    },
+    {
+        word: "chess",
+        hint: "Related to an Indoor Game"
+    },
+    {
+        word: "viber",
+        hint: "A Social Media app"
+    },
+    {
+        word: "github",
+        hint: "Code Hosting Platform"
+    },
+    {
+        word: "png",
+        hint: "A Image File Format"
+    },
+    {
+        word: "silver",
+        hint: "Precious Greyish-White Metal"
+    },
+    {
+        word: "mobile",
+        hint: "An Electronic Device"
+    },
+    {
+        word: "gpu",
+        hint: "Computer Component"
+    },
+    {
+        word: "java",
+        hint: "Programming Language"
+    },
+    {
+        word: "google",
+        hint: "Famous Search Engine"
+    },
+    {
+        word: "venice",
+        hint: "Famous City of Waters"
+    },
+    {
+        word: "excel",
+        hint: "Microsoft Product for Windows"
+    },
+    {
+        word: "mysql",
+        hint: "A Relational Database System"
+    },
+    {
+        word: "nepal",
+        hint: "Developing Country Name"
+    },
+    {
+        word: "flute",
+        hint: "A Musical Instrument"
+    },
+    {
+        word: "crypto",
+        hint: "Related to Crypto Currency"
+    },
+    {
+        word: "tesla",
+        hint: "Unit of Magnetic Flux Density"
+    },
+    {
+        word: "mars",
+        hint: "Planet of our Solar System"
+    },
+    {
+        word: "proxy",
+        hint: "Related to Server Application"
+    },
+    {
+        word: "email",
+        hint: "Related to Exchanging Message"
+    },
+    {
+        word: "html",
+        hint: "Markup Language for the Web"
+    },
+    {
+        word: "air",
+        hint: "Related to a Gas"
+    },
+    {
+        word: "idea",
+        hint: "A Thought or Suggestion"
+    },
+    {
+        word: "server",
+        hint: "Related to Computer or System"
+    },
+    {
+        word: "svg",
+        hint: "A Vector Image Format"
+    },
+    {
+        word: "jpeg",
+        hint: "A Image File Format"
+    },
+    {
+        word: "search",
+        hint: "Act to Find Something"
+    },
+    {
+        word: "key",
+        hint: "Small Piece of Metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "Rsychological Thriller Film"
+    },
+    {
+        word: "dubai",
+        hint: "Developed Country Name"
+    },
+    {
+        word: "photo",
+        hint: "Representation of Person or Scene"
+    },
+    {
+        word: "nile",
+        hint: "Largest River in the World"
+    },
+    {
+        word: "rain",
+        hint: "Related to a Water"
+    },
+]
